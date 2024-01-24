3f2c86f9fd2ba1fbbae138aad60add07aa57e74a
DYAlex <yvette.demidova@yandex.ru>
2024-01-24 20:49:27 +0300

    Added method to get connection to DB

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/*
+должен содержать логику настройки соединения с базой данных
+ */
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/pp_1_1_3_JDBC";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "23102310";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    DB_URL,
+                    DB_USERNAME, DB_PASSWORD);
+//            System.out.println("Connection OK");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR: " + e.getMessage());
+        }
+        return connection;
+    }
 }
