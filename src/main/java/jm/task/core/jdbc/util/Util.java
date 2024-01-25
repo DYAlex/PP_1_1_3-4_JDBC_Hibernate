f211e3e9d8d8e029c8549ad7768adc2a8ac6a4f7
DYAlex <yvette.demidova@yandex.ru>
2024-01-25 11:39:25 +0300

    Added in Util: Hibernate connection to database based on properties and session factory

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,14 +1,21 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/pp_1_1_3_JDBC";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "23102310";
-
     private static Connection connection;
 
     public static Connection getConnection() {
@@ -28,5 +35,42 @@ public class Util {
         } catch (SQLException e) {
             System.err.println("ERROR closing connection: " + e.getMessage());
         }
+    }
+
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = getHibernateConfiguration();
+
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            } catch (Exception e) {
+                System.err.println("ERROR creating session failed: " + e.getMessage());
+            }
+        }
+        return sessionFactory;
+    }
+
+    private static Configuration getHibernateConfiguration() {
+        Configuration configuration = new Configuration();
+
+        Properties settings = new Properties();
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(Environment.URL, DB_URL);
+        settings.put(Environment.USER, DB_USERNAME);
+        settings.put(Environment.PASS, DB_PASSWORD);
+
+        settings.put(Environment.SHOW_SQL, "true");
+
+        settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+        configuration.setProperties(settings);
+        return configuration;
     }
 }
